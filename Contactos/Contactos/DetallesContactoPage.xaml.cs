a808acfb939f6368759e2e4ecf195deb656d8452
Eduardo Rosas <lalorosas@outlook.com>
2017-12-21 23:18:43 -0600

    Add project files.

---
+++ Contactos/Contactos/DetallesContactoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contactos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetallesContactoPage : ContentPage
+	{
+		public DetallesContactoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
