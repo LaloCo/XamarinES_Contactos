915fd3f5f48ea6fe5105ded8b9a645a4fe12d4d1
Eduardo Rosas <lalorosas@outlook.com>
2017-12-26 20:43:46 -0600

    Insertando a una base de datos, que inlcuye la definicion de la clase necesaria para crear la tabla y las entradas.

---
+++ Contactos/Contactos/Clases/Contactos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contactos.Clases
+{
+    public class Contacto
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string apellido;
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private string telefono;
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+    }
+}
--- Contactos/Contactos/NuevoContactoPage.xaml.cs
+++ Contactos/Contactos/NuevoContactoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contactos.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,22 @@ namespace Contactos
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs args)
+        {
+            Contacto nuevoContacto = new Contacto()
+            {
+                Nombre = nombreEntry.Text,
+                Apellido = apellidoEntry.Text,
+                Telefono = telefonoEntry.Text,
+                Email = emailEntry.Text
+            };
+
+            using (var conn = new SQLite.SQLiteConnection(App.RUTA_DB))
+            {
+                conn.CreateTable<Contacto>();
+                conn.Insert(nuevoContacto);
+            }
+        }
+    }
 }
