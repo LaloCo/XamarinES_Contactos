de66a087da86dfb28b3339c456b0114b11a00e18
Eduardo Rosas <lalorosas@outlook.com>
2017-12-26 21:35:53 -0600

    Leyendo de la base de datos y navegando a los detalles.

--- Contactos/Contactos/DetallesContactoPage.xaml.cs
+++ Contactos/Contactos/DetallesContactoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contactos.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,16 @@ namespace Contactos
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DetallesContactoPage : ContentPage
 	{
-		public DetallesContactoPage ()
+        public Contacto Contacto { get; set; }
+        public DetallesContactoPage (Contacto contacto)
 		{
 			InitializeComponent ();
+
+            Contacto = contacto;
+
+            nombreLabel.Text = $"{Contacto.Nombre} {Contacto.Apellido}";
+            telefonoLabel.Text = Contacto.Telefono;
+            correoLabel.Text = Contacto.Email;
 		}
 	}
 }
--- Contactos/Contactos/MainPage.xaml.cs
+++ Contactos/Contactos/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contactos.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,35 @@ namespace Contactos
 {
 	public partial class MainPage : ContentPage
 	{
+        List<Contacto> contactos;
 		public MainPage()
 		{
 			InitializeComponent();
+
+            contactos = new List<Contacto>();
+
+            contactosListView.ItemSelected += ContactosListView_ItemSelected;
 		}
+
+        private void ContactosListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var contactoSeleccionado = e.SelectedItem as Contacto;
+
+            Navigation.PushAsync(new DetallesContactoPage(contactoSeleccionado));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (var conn = new SQLite.SQLiteConnection(App.RUTA_DB))
+            {
+                conn.CreateTable<Contacto>();
+                contactos = conn.Table<Contacto>().ToList();
+
+                contactosListView.ItemsSource = contactos;
+            }
+        }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
